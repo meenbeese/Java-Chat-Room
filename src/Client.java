c4735b459e1ebdaec9f351ba7d98153c304abfc5
meenbeese <meenbeese@tutanota.com>
2023-09-13 18:36:16 -0400

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client implements Runnable {
+
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done;
+
+    @Override
+    public void run() {
+        try {
+            Socket client = new Socket("127.0.0.1", 9999);
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (IOException e) {
+            // TODO: HANDLE
+        }
+    }
+
+    class InputHandler implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String message =
+                }
+            } catch (IOException e) {
+                // handle
+            }
+        }
+    }
+}
